61268ae3c8e84e991b8d050dfd49fcef5b8f933e
Abdul Wajid Korkani <143551139+wajidkorkani@users.noreply.github.com>
2025-11-09 22:15:40 +0500

    Create Word document with heading and paragraph

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
+from docx import Document
 
-print("Hello world!")
+doc = Document()
+doc.add_heading("My First Word File", level=1)
+doc.add_paragraph("This line was typed by Python.")
+doc.save("my_file.docx")
